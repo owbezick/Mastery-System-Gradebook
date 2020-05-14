71a149a15f196635998b1a65673c98e2579acb13
Owen Bezick <owbezick@davidson.edu>
2020-05-13 21:18:54 -0400

    init commit

---
+++ app.R
@@ -0,0 +1,43 @@
+#
+# Author: Owen Bezick
+
+# Source Libraries
+source("libraries.R", local = TRUE)
+
+# Define UI
+ui <- dashboardPage(
+  dashboardHeader(title = "Student View"
+  )
+  , dashboardSidebar(
+    sidebarMenu(
+      menuItem(tabName = "studentProfile", text = "Student Profile", icon = icon("user")) 
+      , menuItem(tabName = "viewGrades", text = "View Grades", icon = icon("check-double"))
+      , menuItem(tabName = "gradeCalculator", text = "Grade Calculator", icon = icon("calculator")))
+  )
+  , dashboardBody( 
+    tabItems(
+      tabItem(
+        tabName = "studentProfile"
+      )
+      , tabItem(
+        tabName = "viewGrades"
+        , tabBox(
+          tabPanel(title = "Exam Grades")
+          , tabPanel(title = "Homework Grades")
+        )
+      )
+      , tabItem(
+        tabName = "gradeCalculator"
+      )
+    )
+  )
+)
+
+
+# Define server logic 
+server <- function(input, output) {
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ libraries.R
@@ -0,0 +1,21 @@
+# Libraries 
+
+# Shiny
+library(shinydashboard)
+library(shinydashboardPlus)
+library(shinyWidgets)
+
+# Data
+library(DT)
+library(dplyr)
+library(lubridate)
+library(openxlsx)
+library(tidyverse)
+library(readxl)
+
+# Viz
+library(echarts4r)
+
+# SQL Drivers
+library(odbc)
+library(DBI)
